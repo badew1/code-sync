74443a9205363d3da6cf07b4fa9735328ff1ed3d
Nicholas H <161533611+badew1@users.noreply.github.com>
2025-07-04 10:41:02 -0400

    Add solution for binary-search

---
+++ leetcode/binary-search.py
@@ -0,0 +1,16 @@
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        if len(nums) == 1:
+            return 0 if nums[0] == target else -1
+        l = 0
+        r = len(nums) - 1
+
+        while l <= r:
+            mid = (l + r) // 2
+            if nums[mid] > target:
+                r = mid - 1
+            elif nums[mid] < target:
+                l = mid + 1
+            else:
+                return mid
+        return -1
